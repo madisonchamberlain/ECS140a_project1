bdd1261ed66d1912cbc17b154dc5d2043488aa82
mpchambe <mpchambe@ad3.ucdavis.edu@pc32.cs.ucdavis.edu>
2021-04-12 14:43:08 -0700

    triangle done

--- triangle/triangle_test.go
+++ triangle/triangle_test.go
@@ -9,8 +9,22 @@ func TestGetTriangleType(t *testing.T) {
 	}
 
 	var tests = []Test{
+		// Unknown = where one side is very large
 		{1001, 5, 6, UnknownTriangle},
-		// TODO add more tests for 100% test coverage
+		{10, 2001, 6, UnknownTriangle},
+		{10, 20, 3001, UnknownTriangle},
+		// Unknown = where one side is less than/equal to 0
+		{0, 5, 6, UnknownTriangle},
+		{10, 0, 6, UnknownTriangle},
+		{10, 20, 0, UnknownTriangle},
+		// Right = a^2 + b^2 = c^2
+		{3, 4, 5, RightTriangle},
+		// Acute = a^2 + b^2 > c^2
+		{3, 4, 2, AcuteTriangle},
+		// Invalid = when sides dont connect
+		{1, 2, 3, InvalidTriangle},
+		// Obtuse = a^2 + b^2 < c^2
+		{3, 4, 6, ObtuseTriangle},
 	}
 
 	for _, test := range tests {
