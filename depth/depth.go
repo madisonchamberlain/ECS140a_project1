856c5db6440df11e9cc16aedffcbd459a01e6ff6
mpchambe <mpchambe@ad3.ucdavis.edu@pc32.cs.ucdavis.edu>
2021-04-13 15:46:32 -0700

    depth finished

--- depth/depth.go
+++ depth/depth.go
@@ -7,6 +7,34 @@ import (
 // Depth should return the maximum number of AST nodes between the root of the
 // tree and any leaf (literal or variable) in the tree.
 func Depth(e expr.Expr) uint {
-	// TODO: implement this function
-	return 1234
+	var depth uint = 1
+	// find out what type e is...
+
+	switch e.(type) {
+	// if e is a Literal its depth is 1
+	case expr.Literal:
+		return depth
+	// if e is a variable its depth is 1
+	case expr.Var:
+		return depth
+	// if e is a unary its depth is 1 + Depth(Expression)
+	case expr.Unary:
+		e := e.(expr.Unary)
+		// recurse on the expression portion
+		return depth + Depth(e.X)
+	case expr.Binary:
+		e := e.(expr.Binary)
+		// recurse on the both expression portions
+		x := Depth(e.X)
+		y := Depth(e.Y)
+		// add the larger side to the current depth count 
+		if x > y{
+			return depth + x
+		} else {
+			return depth + y
+		}
+	// if its none of those types then panic
+	default:
+		panic("unsupported expression")
+	}
 }
