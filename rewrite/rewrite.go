89e6a7cc5c4cd6028699a1406d892bd7996c41cb
mpchambe <mpchambe@ad3.ucdavis.edu@pc32.cs.ucdavis.edu>
2021-04-14 14:21:34 -0700

    update but not done

--- rewrite/rewrite.go
+++ rewrite/rewrite.go
@@ -6,13 +6,50 @@ import (
 	"go/format"
 	"go/parser"
 	"go/token"
-	//"strconv" // This package may be helpful...
+	"hw1/expr"
+	"hw1/simplify"
+	//"log"
+	//"strconv"
 )
+
+// check the package is expr
 
 // rewriteCalls should modify the passed AST
 func rewriteCalls(node ast.Node) {
-	//TODO Write the rewriteCalls function
-	panic("TODO: implement this!")
+	ast.Inspect(node, func(node ast.Node) bool {
+		switch e := node.(type) {
+		// must be an expression call
+		case *ast.CallExpr:
+			// must be a function call
+			switch fun := e.Fun.(type) {
+			case *ast.Ident:
+				// check that the function name is ParseAndEval
+				if fun.Name == "ParseAndEval" {
+					//ensure two elements in the argument list
+					if len(e.Args) == 2 {
+						e.Args[0] = e.Args[1]
+						switch arg := e.Args[0].(type) {
+						case *ast.BasicLit:
+							asString := arg.Value
+							// convert the string to an expression
+							expression, err := expr.Parse(asString)
+							if err == nil {
+								// call simplify to simplify the expression
+								//simp := simplify.Simplify(expression, e.Args[1])
+								simp := simplify.Simplify(expression, e.Args[1].(expr.Env))
+
+								// convert back to a string(expr.Format)
+								str := expr.Format(simp)
+								// set the new arg value
+								e.Args[0].(*ast.BasicLit).Value = str
+							}
+						}
+					}
+				}
+			}
+		}
+		return true
+	})
 }
 
 func SimplifyParseAndEval(src string) string {
