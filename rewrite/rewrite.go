af4a6ba853bde8614861f07a2c4d86882d0b82a2
mpchambe <mpchambe@ad3.ucdavis.edu@pc15.cs.ucdavis.edu>
2021-04-19 16:57:06 -0700

    all works except pkg check

--- rewrite/rewrite.go
+++ rewrite/rewrite.go
@@ -8,10 +8,10 @@ import (
 	"go/token"
 	"hw1/expr"
 	"hw1/simplify"
-	//"log"
 	"strconv"
 )
 
+// check the package is expr
 
 // rewriteCalls should modify the passed AST
 func rewriteCalls(node ast.Node) {
@@ -22,30 +22,21 @@ func rewriteCalls(node ast.Node) {
 			// must be a function call
 			switch fun := e.Fun.(type) {
 			case *ast.Ident:
-				// check that the function name is ParseAndEval 
-				// check the package is expr
-				if fun.Name == "ParseAndEval" && fun.Package == "expr"{
+				// check that the function name is ParseAndEval
+				if fun.Name == "ParseAndEval"{
 
-					// fun.Name = "Changed"
-
-					//ensure two elements in the argument list 
 					if len(e.Args) == 2 {
-
-						//e.Args[0] = e.Args[1]
 
 						switch arg := e.Args[0].(type) {
 						case *ast.BasicLit:
 							// the argument is a literal, but we have to check that it is a string 
 							if arg.Kind == token.STRING{
 								
-								//fmt.Printf(arg.Value)
-								// shoud look like: "1 + 2"
+								asString, error := strconv.Unquote(arg.Value)
 
-								// need to remove the quotes before 
-								asString, error = strconv.Unquote(arg.Value)
+								if error == nil {
 
-								if error == nil{
-									// parse to expression format 
+									// error somewhere in here 
 									expression, err := expr.Parse(asString)
 									if err == nil{
 										// simplify the expression 
@@ -58,11 +49,9 @@ func rewriteCalls(node ast.Node) {
 										withQuotes := strconv.Quote(str)
 
 										// assign the value back to the argument
-										arg.Value = str
+										arg.Value = withQuotes
 									}
-
 								}
-							}
 							}
 						}
 					}
